ee71a7ac287eeb7202e65101b7e496f2402fe1eb
ratnamGT <ratnam.gupta@graytitude.com>
2024-06-21 17:14:58 +0530

    Update hello.cbl test4

--- cobol/hello.cbl
+++ cobol/hello.cbl
@@ -4,5 +4,5 @@
        PROCEDURE DIVISION.
            DISPLAY 'HELLO WORLD'.
            DISPLAY 'HELLO IKAN from GT'.
-           DISPLAY 'Jenkin tetsing123'.   
+           DISPLAY 'Jenkin tetsing1234'.   
            STOP RUN.
