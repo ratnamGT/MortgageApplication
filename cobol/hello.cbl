42d8ac1cc0ca627d4eaeabb9a10022148863f587
ratnamGT <ratnam.gupta@graytitude.com>
2024-07-02 10:31:51 +0530

    Update hello.cbl test

--- cobol/hello.cbl
+++ cobol/hello.cbl
@@ -2,5 +2,5 @@
        PROGRAM-ID. HELLO.
        ENVIRONMENT DIVISION.
        PROCEDURE DIVISION.
-           DISPLAY 'HELLO IKAN from GT'.           
+           DISPLAY 'HELLO IKAN from GT1'.           
            STOP RUN.
