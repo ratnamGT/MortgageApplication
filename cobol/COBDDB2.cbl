c922982e97d36eb4d93308bf4fe460bc39b92f4f
ratnamGT <ratnam.gupta@graytitude.com>
2024-07-03 15:10:01 +0530

    Update COBDDB2.cbl jenk

--- cobol/COBDDB2.cbl
+++ cobol/COBDDB2.cbl
@@ -5,7 +5,7 @@
 
          01  WS-SQLREAD           PIC X(1).
 
-      *** INCLUDING SQLCA COPYBOOK***itest******
+      *** INCLUDING SQLCA COPYBOOK***ijenkt******
            EXEC SQL
                 INCLUDE SQLCA
            END-EXEC.
