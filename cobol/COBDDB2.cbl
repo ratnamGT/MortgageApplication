64cb1350d9ba2836ae663430ade3778bae01e79e
rene devleeschauwer <ratnam.gupta@graytitude.com>
2024-07-08 03:13:33 -0500

    testing pipeline

--- cobol/COBDDB2.cbl
+++ cobol/COBDDB2.cbl
@@ -5,7 +5,7 @@
 
          01  WS-SQLREAD           PIC X(1).
 
-      *** INCLUDING SQLCA COPYBOOK***ijenk******
+      *** INCLUDING SQLCA COPYBOOK**jenkins******
            EXEC SQL
                 INCLUDE SQLCA
            END-EXEC.
