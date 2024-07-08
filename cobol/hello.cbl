5e37d978e74c50df4433684a36e853a1082711fa
rene devleeschauwer <ratnam.gupta@graytitude.com>
2024-07-08 04:26:11 -0500

    hello changes

--- cobol/hello.cbl
+++ cobol/hello.cbl
@@ -2,5 +2,5 @@
        PROGRAM-ID. HELLO.
        ENVIRONMENT DIVISION.
        PROCEDURE DIVISION.
-           DISPLAY 'HELLO IKAN testing pipeline1'.           
+           DISPLAY 'HELLO IKAN from Gyatitude'.           
            STOP RUN.
