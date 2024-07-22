8d7dd89cd53e88331e109a8efd5815e3b2a76b7a
ratnamGT <ratnam.gupta@graytitude.com>
2024-07-22 14:59:32 +0530

    Update hello.cbl

--- cobol/hello.cbl
+++ cobol/hello.cbl
@@ -2,5 +2,5 @@
        PROGRAM-ID. HELLO.
        ENVIRONMENT DIVISION.
        PROCEDURE DIVISION.
-           DISPLAY 'HELLO IKAN from Gyatitude wazi1'.           
+           DISPLAY 'HELLO IKAN from Gyatitude wazi deploy'.           
            STOP RUN.
