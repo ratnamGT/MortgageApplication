aa1ea8c8dd05ab59e0d845faa7269c87bb03fb31
ratnamGT <ratnam.gupta@graytitude.com>
2024-07-23 13:20:49 +0530

    Update heloword.cbl

--- cobol/heloword.cbl
+++ cobol/heloword.cbl
@@ -2,5 +2,5 @@
        PROGRAM-ID. HELOWORD.
        ENVIRONMENT DIVISION.
        PROCEDURE DIVISION.
-           DISPLAY 'HELLO WORLD GT2344'.           
+           DISPLAY 'HELLO WORLD GT234'.           
            STOP RUN.
