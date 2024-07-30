fdbad0f6e3bc3c075fd2549f1ef649f8a02e18f2
ratnamGT <ratnam.gupta@graytitude.com>
2024-07-30 13:57:48 +0530

    Update heloword.cbl

--- cobol/heloword.cbl
+++ cobol/heloword.cbl
@@ -2,5 +2,5 @@
        PROGRAM-ID. HELOWORD.
        ENVIRONMENT DIVISION.
        PROCEDURE DIVISION.
-           DISPLAY 'HELLO ikan FROM gt'.           
+           DISPLAY 'HELLO ikan FROM GT'.           
            STOP RUN.
