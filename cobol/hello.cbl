00bdda72e4cbef6c2c728cfe241ee8c39b6d479f
ratnamGT <ratnam.gupta@graytitude.com>
2024-12-04 20:20:18 +0530

    test

--- cobol/hello.cbl
+++ cobol/hello.cbl
@@ -1,7 +1,7 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. HELLO.
        ENVIRONMENT DIVISION.
-      * 
+      *
        PROCEDURE DIVISION.
-           DISPLAY 'Heyyy Trisha Welcome to GMS'.
+           DISPLAY 'Hi Trisha Welcome to GMS'.
            STOP RUN.
