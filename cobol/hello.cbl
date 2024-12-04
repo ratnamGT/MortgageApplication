3a7db0e454a7afa168c89a98344121f427ee114c
ratnamGT <ratnam.gupta@graytitude.com>
2024-12-04 20:23:34 +0530

    test

--- cobol/hello.cbl
+++ cobol/hello.cbl
@@ -3,5 +3,5 @@
        ENVIRONMENT DIVISION.
       *
        PROCEDURE DIVISION.
-           DISPLAY 'Hi Trisha Welcome to GMS'.
+           DISPLAY 'Hello Trisha Welcome to GMS'.
            STOP RUN.
