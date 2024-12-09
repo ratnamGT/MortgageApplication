fa805b18d8a507383e4292b7c661fbd011d3dc77
Thrisha020 <thrisha@genairesonance.com>
2024-12-09 14:45:46 +0530

    changed

--- cobol/hello.cbl
+++ cobol/hello.cbl
@@ -1,7 +1,7 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. HELLO.
        ENVIRONMENT DIVISION.
-      *hyeee
+      *hyeeeeee
        PROCEDURE DIVISION.
            DISPLAY 'Hello impact build testingggg'.
            STOP RUN.
