6adf1fb95449f23e943d81b174460c48f8bf76c1
rene devleeschauwer <ratnam.gupta@graytitude.com>
2024-12-17 08:47:15 -0600

    restore ADCDMST.DBBDEV.LOAD

--- cobol/COBDDB2.cbl
+++ cobol/COBDDB2.cbl
@@ -4,7 +4,7 @@
          WORKING-STORAGE SECTION.
 
          01  WS-SQLREAD           PIC X(1).
-      *** IKAN DEMO with DBB *** 
+      *** IKAN DEMO with DBB with IBM *** 
       *** INCLUDING SQLCA COPYBOOK ******
            EXEC SQL
                 INCLUDE SQLCA
