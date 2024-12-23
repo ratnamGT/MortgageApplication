70f2ab9b401ddecbd62532e2b1dd84f8cab681bc
Thrisha020 <thrisha@genairesonance.com>
2024-12-23 13:42:26 +0530

    procedure division changed

--- cobol/hello.cbl
+++ cobol/hello.cbl
@@ -3,5 +3,5 @@
        ENVIRONMENT DIVISION.
       *hyeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
        PROCEDURE DIVISION.
-           DISPLAY 'Hello impact build testing12345'.
+           DISPLAY 'Hello impact build testing12345676'.
            STOP RUN.
