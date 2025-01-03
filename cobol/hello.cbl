3f6cee5a4fe5f916742518909c7f4bc9bbb4be7a
Thrisha020 <thrisha@genairesonance.com>
2025-01-03 17:54:29 +0530

    yegwtegb

--- cobol/hello.cbl
+++ cobol/hello.cbl
@@ -3,5 +3,5 @@
        ENVIRONMENT DIVISION.
       *hyeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
        PROCEDURE DIVISION.
-           DISPLAY 'Hello impact build cdo_fdf'.
+           DISPLAY 'Hello impacttthht build cdo_fdf'.
            STOP RUN.
